0b9f5175e90fcfe0f76153dcad2d486178becee3
LucaCassani5CI <luca.cassani@alessandrinimainardi.edu.it>
2023-02-27 09:05:47 +0100

    giornalisti articoli api

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, ArticoloDetailViewCB, ArticoloListView, GiornalistaDetailViewCB, GiornalistaListView #articoloDetailView
+from .views import home, ArticoloDetailViewCB, ArticoloListView, GiornalistaDetailViewCB, GiornalistaListView, giornalisti_list_api, giornalisti_api, articoli_list_api, articoli_api #articoloDetailView
 
 app_name = 'news'
 
@@ -10,4 +10,9 @@ urlpatterns = [
     path("lista_articoli/", ArticoloListView.as_view(), name="lista_articoli"),
     path("giornalisti/<int:pk>", GiornalistaDetailViewCB.as_view(), name="giornalista_detail"),
     path("lista_giornalisti/", GiornalistaListView.as_view(), name="lista_giornalisti"),
+    path("lista_giornalisti_api/",giornalisti_list_api, name="giornalisti_list_api"),
+    path("giornalisto_api/<int:pk>", giornalisti_api, name="giornalisti_api"),
+    path("lista_articoli_api/",articoli_list_api, name="articoli_list_api"),
+    path("articolo_api/<int:pk>", articoli_api, name="articoli_api"),
+
 ]
--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Application definition
 
@@ -43,7 +45,8 @@ INSTALLED_APPS = [
     'news',
     'prova_pratica_1',
     'forms_app',
-    'crispy_forms'
+    'crispy_forms',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'primo_progetto.urls'
